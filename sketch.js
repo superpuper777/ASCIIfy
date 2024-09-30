60c738358e7863085dabaa190c0f39a4f856eb46
alena-budnik <alena.budnik@opstalent.com>
2024-09-30 23:45:08 +0300

    init: all features implemented

---
+++ sketch.js
@@ -0,0 +1,86 @@
+let img;
+let fileInput;
+let downloadButton;
+let fakeButton;
+
+function setup() {
+  fileInput = createFileInput(handleFile);
+  fileInput.position(-1000, -1000);
+
+  fakeButton = createButton('Загрузить файл');
+  fakeButton.position(10, 10);
+
+  fakeButton.style('background-color', 'transparent');
+  fakeButton.style('color', '#fff');
+  fakeButton.style('padding', '10px 20px');
+  fakeButton.style('border', '1px solid white');
+  fakeButton.style('border-radius', '5px');
+  fakeButton.style('cursor', 'pointer');
+
+  fakeButton.mousePressed(() => fileInput.elt.click());
+
+  noLoop();
+
+  downloadButton = createButton('Скачать изображение');
+  downloadButton.position(10, 70, 'fixed');
+  downloadButton.hide();
+  downloadButton.mousePressed(saveImage);
+}
+
+function draw() {
+  clear();
+  if (img) {
+    img.loadPixels();
+    let pixelSize = 8;
+    let spacing = 2;
+
+    const density = 'Ñ@#W$9876543210?!abc;:+=-,._  ';
+    for (let y = 0; y < img.height; y += pixelSize + spacing) {
+      for (let x = 0; x < img.width; x += pixelSize + spacing) {
+        let c = img.get(x, y);
+        fill(c);
+        noStroke();
+
+        let avg = (red(c) + green(c) + blue(c)) / 3;
+        let charIndex = floor(map(avg, 0, 255, density.length - 1, 0));
+        let asciiChar = density.charAt(charIndex);
+
+        textSize(pixelSize);
+        textAlign(CENTER, CENTER);
+        fill(c);
+        text(
+          asciiChar,
+          x + (pixelSize + spacing) / 2,
+          y + (pixelSize + spacing) / 2
+        );
+
+        stroke(0);
+        strokeWeight(1);
+        text(
+          asciiChar,
+          x + (pixelSize + spacing) / 2,
+          y + (pixelSize + spacing) / 2
+        );
+        noStroke();
+      }
+    }
+  }
+}
+
+function handleFile(file) {
+  if (file && file.type === 'image') {
+    img = loadImage(file.data, () => {
+      let imgWidth = img.width;
+      let imgHeight = img.height;
+
+      resizeCanvas(imgWidth, imgHeight);
+      redraw();
+
+      downloadButton.show();
+    });
+  }
+}
+
+function saveImage() {
+  saveCanvas('pixel_image', 'png');
+}
